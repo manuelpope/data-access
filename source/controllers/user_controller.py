44678e125237971ff8a25cf2c369f90199e05be9
manueltobon <manuelpope@gmail.com>
2023-07-04 19:49:44 -0300

    version 1.2

--- source/controllers/user_controller.py
+++ source/controllers/user_controller.py
@@ -1,5 +1,5 @@
 # controllers/user_controller.py
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from services.user_service import UserService
 
 
@@ -12,14 +12,26 @@ class UserController:
         self.router.get("/users/{user_id}")(self.get_user)
         self.router.put("/users/{user_id}")(self.update_user)
 
-    def create_user(self, user_data: dict):
-        user = self.user_service.create_user(user_data)
-        return {"message": "User created successfully", "user": user}
+    async def create_user(self, user_data: dict, user_service: UserService = Depends()):
+        user = await user_service.create_user(user_data)
+        if user:
+            return {"message": "User created successfully", "user": user}
 
-    def get_user(self, user_id: str):
-        user = self.user_service.get_user(user_id)
-        return {"user": user}
+        raise HTTPException(status_code=400, detail="User already exists")
 
-    def update_user(self, user_id: str, user_data: dict):
-        user = self.user_service.update_user(user_id, user_data)
-        return {"message": "User updated successfully", "user": user}
+    async def get_user(self, user_id: str, user_service: UserService = Depends()):
+        user = await user_service.get_user(user_id)
+        if user:
+            return {"user": user}
+
+        raise HTTPException(status_code=404, detail="User not found")
+
+    async def update_user(self, user_id: str, user_data: dict, user_service: UserService = Depends()):
+        try:
+            user = await user_service.update_user(user_id, user_data)
+            if user:
+                return {"message": "User updated successfully", "user": user}
+            else:
+                raise HTTPException(status_code=404, detail="User not found")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
--- source/services/user_service.py
+++ source/services/user_service.py
@@ -11,11 +11,11 @@ class UserService:
         else:
             self.database = InMemoryDatabase()
 
-    def create_user(self, user_data):
-        return self.database.create_user(user_data)
+    async def create_user(self, user_data):
+        return await self.database.create_user(user_data)
 
-    def get_user(self, user_id):
-        return self.database.get_user(user_id)
+    async def get_user(self, user_id):
+        return await self.database.get_user(user_id)
 
-    def update_user(self, user_id, user_data):
-        return self.database.update_user(user_id, user_data)
+    async def update_user(self, user_id, user_data):
+        return await self.database.update_user(user_id, user_data)
